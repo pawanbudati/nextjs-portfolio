8dcaa47a87a49dda0743580232363dcf1872c6f7
pawanbudati <pawan1122143@gmail.com>
2025-02-06 23:49:36 +0530

    Initial commit

---
+++ utils/data/experience.js
@@ -0,0 +1,16 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Developer',
+    company: "GalaxE Solutions Ind.",
+    duration: "Jan 2024 - Present",
+    offiseSite: "https://www.galaxe.com/"
+  },
+  {
+    id: 2,
+    title: 'Associate Developer',
+    company: "GalaxE Solutions Ind.",
+    duration: "Jul 2022 - Jan 2024",
+    offiseSite: "https://www.galaxe.com/"
+  },
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,13 @@
+export const personalData = {
+  name: "Pavan Budati",
+  profile: '/profile2.png',
+  designation: "Software Developer",
+  description: "My name is Pavan Kumar Budati. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skills are based on Java and JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'pawanbudati@gmail.com',
+  phone: '+91 9154899219',
+  address: 'Whitefield, Bangalore - 560067',
+  github: 'https://github.com/pawanbudati',
+  linkedIn: 'https://www.linkedin.com/in/pavanbudati/',
+  twitter: 'https://twitter.com/pawanbudati',
+  resume: "https://docs.google.com/document/d/1bC4kdTkhbmsrrjyd1AjHro53wUUcoPckIqwGAh72QNA/edit?usp=sharing"
+}
